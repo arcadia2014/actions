d1070a6fde5f06e5d5c574ce3bc67e7e820bb0a3
alfredocorona16 <alfredcorona16@gmail.com>
2026-06-17 15:37:37 -0600

    Updating actions for python

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-print("Hello, World! runs on Actions")
+import os
 
-for i in [1, 2, 3, 4, 5]:
-    print(f"XD" * i)
+def main():
+    print(f"Hello World! runs on actions")
+
+    for i in [1, 2, 3]:
+        print(f"XD"* i)
+        
+    name = os.getenv("USERNAME")
+    print(f"Hello {name}")
+    
+    
+    
+if __name__ == "__main__":
+    main()
     
